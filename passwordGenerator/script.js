5d13dcccfe6c31b401d4a046be1c1ed5d100c49a
Ahda-Rizqi <AhdaRizqi1199@gmail.com>
2025-04-18 08:36:24 +0700

    project generate Password #1

---
+++ passwordGenerator/script.js
@@ -0,0 +1,46 @@
+// variabel tombol
+const tombol = document.querySelector(".tombol");
+
+// variabel
+const opsi = Array.from(document.querySelectorAll(".opsi"));
+
+// variabel hasil
+const hasil = document.querySelector(".hasil");
+
+// array password
+let karakterPassword = {
+  hurufKecil: "abcdefghijklmnopqrstuvwxyz".split(""),
+  hurufBesar: "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""),
+  angka: "0123456789".split(""),
+  simbol: "!@#$%^&*()_+".split(""),
+};
+let karakterDipilih = [];
+
+// array hasil
+let hasilGenerate = [];
+
+tombol.addEventListener("click", function () {
+  hasilGenerate = [];
+  hasil.classList.remove("hidden");
+
+  // ambil nilai checkBox
+  for (let i = 0; i < opsi.length; i++) {
+    if (opsi[i].checked) {
+      karakterDipilih = karakterDipilih.concat(karakterPassword[opsi[i].id]);
+    }
+
+    if (karakterDipilih.length === 0) {
+      hasil.innerHTML = "Pilih dulu opsi karakter!";
+
+      return;
+    }
+  }
+
+  for (let i = 0; i < 12; i++) {
+    let randomKarakter = Math.floor(Math.random() * karakterDipilih.length);
+    hasilGenerate.push(karakterDipilih[randomKarakter]);
+  }
+  hasil.textContent = hasilGenerate.join("");
+
+  karakterDipilih = [];
+});
