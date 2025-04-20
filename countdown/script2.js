3db8773520fd3fc971169cec167e877264df71a2
Ahda-Rizqi <AhdaRizqi1199@gmail.com>
2025-04-20 19:27:20 +0700

    project countDown #2

--- countdown/script2.js
+++
@@ -1,6 +0,0 @@
-const submit = document.getElementById("submit");
-
-submit.addEventListener("click", function () {
-  let newTime = new Date().getTime();
-  let timeFuture = new Date("2025-04-21").getTime();
-});
